351c15b926ce565d7708574d27e6853f0598ec9c
Jasmin Shrestha <jasmin.shrestha@hazesoft.co>
2022-08-24 18:12:49 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## TODO
+1. create VPC
+    a. PROD
+    b. UAT
+    c. STAGE
+2. create subnet
+    a. private subnet - natgateway
+    b. public subnet  - internet gateway
+3. Two bastion host
+    a. prod - private subnet
+    b. stage + uat  - private subnet
+4. Create ec2 instance in QA-VPC with private IP
+5. public and private LB 
+
---
+++ networking.tf
@@ -0,0 +1,628 @@
+###########
+### VPC ###
+###########
+
+resource "aws_vpc" "QA_VPC" {
+  cidr_block       = "10.10.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "QA_VPC"
+  }
+}
+
+resource "aws_vpc" "UAT_VPC" {
+  cidr_block       = "10.20.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "UAT_VPC"
+  }
+}
+
+resource "aws_vpc" "PROD_VPC" {
+  cidr_block       = "10.30.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "PROD_VPC"
+  }
+}
+
+#########################
+### QA-PUBLIC-SUBNET 1a ###
+#########################
+
+resource "aws_subnet" "QA-PUBLIC-SUBNET-1A" {
+  vpc_id = aws_vpc.QA_VPC.id
+  cidr_block = "10.10.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "QA-PUBLIC-SUBNET-1A"
+  }
+}
+resource "aws_subnet" "QA-PUBLIC-SUBNET-1B" {
+  vpc_id = aws_vpc.QA_VPC.id
+  cidr_block = "10.10.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "QA-PUBLIC-SUBNET-1B"
+  }
+}
+resource "aws_internet_gateway" "QA_VPC_IGW" {
+  vpc_id = aws_vpc.QA_VPC.id
+  tags = {
+    Name = "QA_VPC_IGW"
+  }
+}
+resource "aws_route_table" "QA-PUBLIC-RT-TABLE" {
+    vpc_id = aws_vpc.QA_VPC.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.QA_VPC_IGW.id
+    }
+    tags = {
+        Name = "QA-PUBLIC-ROUTE-TABLE"
+    }
+}
+resource "aws_route_table_association" "QA-PUBLIC-RT-TABLE-1A" {
+    subnet_id = aws_subnet.QA-PUBLIC-SUBNET-1A.id
+    route_table_id = aws_route_table.QA-PUBLIC-RT-TABLE.id
+}
+resource "aws_route_table_association" "QA-PUBLIC-RT-TABLE-1B" {
+    subnet_id = aws_subnet.QA-PUBLIC-SUBNET-1B.id
+    route_table_id = aws_route_table.QA-PUBLIC-RT-TABLE.id
+}
+
+
+#########################
+### QA-PRIVATE-SUBNET ##
+#########################
+resource "aws_subnet" "QA-PRIVATE-SUBNET-1A" {
+  vpc_id = aws_vpc.QA_VPC.id
+  cidr_block = "10.10.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "QA-PRIVATE-SUBNET-1A"
+  }
+}
+resource "aws_subnet" "QA-PRIVATE-SUBNET-1B" {
+  vpc_id = aws_vpc.QA_VPC.id
+  cidr_block = "10.10.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "QA-PRIVATE-SUBNET-1B"
+  }
+}
+
+resource "aws_eip" "QA-NAT-EIP" {
+  vpc = true
+  tags = {
+    Name = "QA-NAT-EIP"
+  }
+}
+
+resource "aws_nat_gateway" "QA-NAT-GW" {
+  allocation_id = aws_eip.QA-NAT-EIP.id
+  subnet_id = aws_subnet.QA-PRIVATE-SUBNET-1A.id
+}
+
+resource "aws_route_table" "QA-PRIVATE-RT-TABLE" {
+    vpc_id = aws_vpc.QA_VPC.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.QA-NAT-GW.id
+    }
+    tags = {
+        Name = "QA-PRIVATE-ROUTE-TABLE"
+    }
+}
+
+resource "aws_route_table_association" "QA-PRIVATE-RT-TABLE-1A" {
+    subnet_id = aws_subnet.QA-PRIVATE-SUBNET-1A.id
+    route_table_id = aws_route_table.QA-PRIVATE-RT-TABLE.id
+}
+resource "aws_route_table_association" "QA-PRIVATE-RT-TABLE-1B" {
+    subnet_id = aws_subnet.QA-PRIVATE-SUBNET-1B.id
+    route_table_id = aws_route_table.QA-PRIVATE-RT-TABLE.id
+}
+
+resource "aws_security_group" "MYSERVER-QA-SG" {  ## Replace MYSERVER-QA-SG with your server name
+  name = "MYSERVER-QA-SG"
+  description = "Allow SSH inbound connections"
+  vpc_id = aws_vpc.QA_VPC.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "MYSERVER-QA-SG"
+  }
+}
+
+#########
+## ASG ##
+#########
+
+resource "aws_launch_configuration" "MYSERVER-QA" {
+  name_prefix = "terraform-"
+
+  image_id = "ami-052efd3df9dad4825" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type = "t1.micro"
+  key_name = "terraform-test"
+  
+  security_groups = [ aws_security_group.MYSERVER-QA-SG.id ]
+  associate_public_ip_address = true
+  user_data = <<EOF
+  apt install nginx
+chkconfig nginx on
+service nginx start
+  EOF 
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_security_group" "ALB-QA-SG" {
+  name        = "ALB-QA-SG"
+  description = "Allow HTTP traffic to instances through Elastic Load Balancer"
+  vpc_id = aws_vpc.QA_VPC.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow HTTP through QA ALB Security Group"
+  }
+}
+
+
+resource "aws_autoscaling_group" "test-asg-group" {
+  name = "${aws_launch_configuration.MYSERVER-QA.name}-asg"
+
+  min_size             = 1
+  desired_capacity     = 2
+  max_size             = 4
+  
+  # health_check_type    = "ELB"
+  # load_balancers = [
+  #   aws_lb.ALB-QA.id
+  # ]
+
+  launch_configuration = aws_launch_configuration.MYSERVER-QA.name
+
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupTotalInstances"
+  ]
+
+  metrics_granularity = "1Minute"
+
+  vpc_zone_identifier  = [
+    aws_subnet.QA-PUBLIC-SUBNET-1A.id,
+    aws_subnet.QA-PUBLIC-SUBNET-1B.id
+  ]
+
+  # Required to redeploy without an outage.
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "terraform"
+    propagate_at_launch = true
+  }
+
+}
+
+resource "aws_lb" "ALB-QA" {
+  name               = "ALB-QA"
+  internal           = false
+  load_balancer_type = "application"
+  
+  security_groups = [
+    aws_security_group.ALB-QA-SG.id
+  ]
+  subnets = [
+    aws_subnet.QA-PUBLIC-SUBNET-1A.id,
+    aws_subnet.QA-PUBLIC-SUBNET-1B.id
+  ]
+  # cross_zone_load_balancing   = true
+
+  # health_check {
+  #   healthy_threshold = 2
+  #   unhealthy_threshold = 2
+  #   timeout = 3
+  #   interval = 30
+  #   target = "HTTP:80/"
+  # }
+
+  # listener {
+  #   lb_port = 80
+  #   lb_protocol = "http"
+  #   instance_port = "80"
+  #   instance_protocol = "http"
+  # }
+
+}
+resource "aws_lb_listener" "ALB-QA-LISTENER" {
+  load_balancer_arn = aws_lb.ALB-QA.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ALB-QA-TARGET.arn
+  }
+}
+
+resource "aws_lb_target_group" "ALB-QA-TARGET" {
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    path = "/"
+    protocol = "HTTP"
+  }
+  
+  name     = "learn-asg-QA"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.QA_VPC.id
+
+  
+}
+
+resource "aws_autoscaling_attachment" "ALB-QA-ATTACHMENT" {
+  autoscaling_group_name = aws_autoscaling_group.test-asg-group.id
+  lb_target_group_arn   = aws_lb_target_group.ALB-QA-TARGET.arn
+}
+
+resource "aws_autoscaling_policy" "web_policy_up" {
+  name = "web_policy_up"
+  scaling_adjustment = 1
+  adjustment_type = "ChangeInCapacity"
+  cooldown = 300
+  autoscaling_group_name = aws_autoscaling_group.test-asg-group.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
+  alarm_name = "web_cpu_alarm_up"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = "2"
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+  period = "120"
+  statistic = "Average"
+  threshold = "60"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.test-asg-group.name
+  }
+
+  alarm_description = "This metric monitor EC2 instance CPU utilization"
+  alarm_actions = [ aws_autoscaling_policy.web_policy_up.arn ]
+}
+
+resource "aws_autoscaling_policy" "web_policy_down" {
+  name = "web_policy_down"
+  scaling_adjustment = -1
+  adjustment_type = "ChangeInCapacity"
+  cooldown = 300
+  autoscaling_group_name = aws_autoscaling_group.test-asg-group.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_down" {
+  alarm_name = "web_cpu_alarm_down"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods = "2"
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+  period = "120"
+  statistic = "Average"
+  threshold = "10"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.test-asg-group.name
+  }
+
+  alarm_description = "This metric monitor EC2 instance CPU utilization"
+  alarm_actions = [ aws_autoscaling_policy.web_policy_down.arn ]
+}
+
+//#########################
+//### UAT-PUBLIC-SUBNET ###
+//#########################
+//
+//resource "aws_subnet" "UAT-PUBLIC-SUBNET" {
+//  vpc_id = aws_vpc.UAT_VPC.id
+//  cidr_block = "10.20.1.0/24"
+//  availability_zone = "us-east-1a"
+//
+//  tags = {
+//    Name = "UAT-PUBLIC-SUBNET"
+//  }
+//}
+//
+//resource "aws_internet_gateway" "UAT_VPC_IGW" {
+//  vpc_id = aws_vpc.UAT_VPC.id
+//
+//  tags = {
+//    Name = "UAT_VPC_IGW"
+//  }
+//}
+//
+//resource "aws_route_table" "UAT-PUBLIC-RT-TABLE" {
+//    vpc_id = aws_vpc.UAT_VPC.id
+//
+//    route {
+//        cidr_block = "0.0.0.0/0"
+//        gateway_id = aws_internet_gateway.UAT_VPC_IGW.id
+//    }
+//
+//    tags = {
+//        Name = "UAT-PUBLIC-ROUTE-TABLE"
+//    }
+//}
+//
+//resource "aws_route_table_association" "UAT-PUBLIC-RT-TABLE" {
+//    subnet_id = aws_subnet.UAT-PUBLIC-SUBNET.id
+//    route_table_id = aws_route_table.UAT-PUBLIC-RT-TABLE.id
+//}
+//
+//#########################
+//### UAT-PRIVATE-SUBNET ##
+//#########################
+//resource "aws_subnet" "UAT-PRIVATE-SUBNET" {
+//  vpc_id = aws_vpc.UAT_VPC.id
+//  cidr_block = "10.20.2.0/24"
+//  availability_zone = "us-east-1a"
+//
+//  tags = {
+//    Name = "UAT-PRIVATE-SUBNET"
+//  }
+//}
+//
+//resource "aws_eip" "UAT-NAT-EIP" {
+//  vpc = true
+//}
+//
+//resource "aws_nat_gateway" "UAT-NAT-GW" {
+//  allocation_id = aws_eip.UAT-NAT-EIP.id
+//  subnet_id = aws_subnet.UAT-PRIVATE-SUBNET.id
+//}
+//
+//resource "aws_route_table" "UAT-PRIVATE-RT-TABLE" {
+//    vpc_id = aws_vpc.UAT_VPC.id
+//
+//    route {
+//        cidr_block = "0.0.0.0/0"
+//        nat_gateway_id = aws_nat_gateway.UAT-NAT-GW.id
+//    }
+//
+//    tags = {
+//        Name = "UAT-PRIVATE-ROUTE-TABLE"
+//    }
+//}
+//
+//resource "aws_route_table_association" "UAT-PRIVATE-RT-TABLE" {
+//    subnet_id = aws_subnet.UAT-PRIVATE-SUBNET.id
+//    route_table_id = aws_route_table.UAT-PRIVATE-RT-TABLE.id
+//}
+//
+//#########################
+//### PROD-PUBLIC-SUBNET ###
+//#########################
+//
+//resource "aws_subnet" "PROD-PUBLIC-SUBNET" {
+//  vpc_id = aws_vpc.PROD_VPC.id
+//  cidr_block = "10.30.1.0/24"
+//  availability_zone = "us-east-1a"
+//
+//  tags = {
+//    Name = "PROD-PUBLIC-SUBNET"
+//  }
+//}
+//
+//resource "aws_internet_gateway" "PROD_VPC_IGW" {
+//  vpc_id = aws_vpc.PROD_VPC.id
+//
+//  tags = {
+//    Name = "PROD_VPC_IGW"
+//  }
+//}
+//
+//resource "aws_route_table" "PROD-PUBLIC-RT-TABLE" {
+//    vpc_id = aws_vpc.PROD_VPC.id
+//
+//    route {
+//        cidr_block = "0.0.0.0/0"
+//        gateway_id = aws_internet_gateway.PROD_VPC_IGW.id
+//    }
+//
+//    tags = {
+//        Name = "PROD-PUBLIC-ROUTE-TABLE"
+//    }
+//}
+//
+//resource "aws_route_table_association" "PROD-PUBLIC-RT-TABLE" {
+//    subnet_id = aws_subnet.PROD-PUBLIC-SUBNET.id
+//    route_table_id = aws_route_table.PROD-PUBLIC-RT-TABLE.id
+//}
+//
+//#########################
+//### PROD-PRIVATE-SUBNET ##
+//#########################
+//resource "aws_subnet" "PROD-PRIVATE-SUBNET" {
+//  vpc_id = aws_vpc.PROD_VPC.id
+//  cidr_block = "10.30.2.0/24"
+//  availability_zone = "us-east-1a"
+//
+//  tags = {
+//    Name = "PROD-PRIVATE-SUBNET"
+//  }
+//}
+//
+//resource "aws_eip" "PROD-NAT-EIP" {
+//  vpc = true
+//}
+//
+//resource "aws_nat_gateway" "PROD-NAT-GW" {
+//  allocation_id = aws_eip.PROD-NAT-EIP.id
+//  subnet_id = aws_subnet.PROD-PRIVATE-SUBNET.id
+//}
+//
+//resource "aws_route_table" "PROD-PRIVATE-RT-TABLE" {
+//    vpc_id = aws_vpc.PROD_VPC.id
+//
+//    route {
+//        cidr_block = "0.0.0.0/0"
+//        nat_gateway_id = aws_nat_gateway.PROD-NAT-GW.id
+//    }
+//
+//    tags = {
+//        Name = "PROD-PRIVATE-ROUTE-TABLE"
+//    }
+//}
+//
+//resource "aws_route_table_association" "PROD-PRIVATE-RT-TABLE" {
+//    subnet_id = aws_subnet.PROD-PRIVATE-SUBNET.id
+//    route_table_id = aws_route_table.PROD-PRIVATE-RT-TABLE.id
+//}
+//
+//#######################
+//### BASTION-HOST_QA ###
+//#######################
+//
+//resource "aws_security_group" "QA-BASTION-HOST-SG" {
+//  name = "QA-BASTION-HOST-SG"
+//  description = "Allow SSH inbound connections"
+//  vpc_id = aws_vpc.QA_VPC.id
+//
+//  ingress {
+//    from_port = 22
+//    to_port = 22
+//    protocol = "tcp"
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
+//  egress {
+//    protocol    = "-1"
+//    from_port   = 0
+//    to_port     = 0
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
+//  tags = {
+//    Name = "QA-BASTION-HOST-SG"
+//  }
+//}
+//
+//resource "aws_instance" "BASTION-HOST_QA" {
+//  ami = "ami-052efd3df9dad4825" # ubuntu image
+//  instance_type = "t1.micro"
+//  key_name = "terraform-test"
+//  availability_zone = "us-east-1a"
+//  vpc_security_group_ids = [ aws_security_group.QA-BASTION-HOST-SG.id ]
+//  subnet_id = aws_subnet.QA-PUBLIC-SUBNET-1A.id
+//  associate_public_ip_address = true
+//
+//  tags = {
+//    Name = "BASTION-HOST_QA"
+//  }
+//}
+//
+//#######################
+//### BASTION-HOST_PROD ###
+//#######################
+//
+//resource "aws_security_group" "PROD-BASTION-HOST-SG" {
+//  name = "PROD-BASTION-HOST-SG"
+//  description = "Allow SSH inbound connections"
+//  vpc_id = aws_vpc.PROD_VPC.id
+//
+//  ingress {
+//    from_port = 22
+//    to_port = 22
+//    protocol = "tcp"
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
+//  egress {
+//    protocol    = "-1"
+//    from_port   = 0
+//    to_port     = 0
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
+//  tags = {
+//    Name = "QA-BASTION-HOST-SG"
+//  }
+//}
+//
+//resource "aws_instance" "BASTION-HOST_PROD" {
+//  ami = "ami-052efd3df9dad4825" # ubuntu image
+//  instance_type = "t1.micro"
+//  key_name = "terraform-test"
+//  availability_zone = "us-east-1a"
+//  vpc_security_group_ids = [ aws_security_group.PROD-BASTION-HOST-SG.id ]
+//  subnet_id = aws_subnet.PROD-PUBLIC-SUBNET.id
+//  associate_public_ip_address = true
+//
+//  tags = {
+//    Name = "BASTION-HOST_PROD"
+//  }
+//}
+//
+//#######################
+//### MYSERVER_QA_INSTANCE ###
+//#######################
+//
+//
+//resource "aws_instance" "MYSERVER-QA" {
+//  ami = "ami-052efd3df9dad4825" # ubuntu image
+//  instance_type = "t1.micro"
+//  key_name = "terraform-test"
+//  availability_zone = "us-east-1a"
+//  vpc_security_group_ids = [ aws_security_group.MYSERVER-QA-SG.id ]
+//  subnet_id = aws_subnet.QA-PRIVATE-SUBNET.id
+//  associate_public_ip_address = false
+//
+//  tags = {
+//    Name = "MYSERVER-QA"
+//  }
+//}
+//
